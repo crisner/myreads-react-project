5cb9d02698b26fced287ec81b7a3d74df39d8610
Renisha Christie. A <christie.reni@gmail.com>
2018-10-12 00:04:09 +0530

    Pass props to Book component

--- src/Book.js
+++ src/Book.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import BookshelfChanger from './BookshelfChanger';
 
-function Book () {
+function Book (props) {
         return (
             <li>
                 <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${ props.url })` }}></div>
                     <BookshelfChanger />
                 </div>
-                <div className="book-title"></div>
-                <div className="book-authors"></div>
+                <div className="book-title">{ props.title }</div>
+                <div className="book-authors">{ props.authors }</div>
                 </div>
             </li>
         );
--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -16,7 +16,10 @@ class Bookshelf extends Component {
                 <h2 className="bookshelf-title">{this.props.title}</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
-                    <Book />
+                    <Book
+                    title="To Kill a Mockingbird"
+                    authors="Harper Lee"
+                    url="http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api" />
                     </ol>
                 </div>
             </div>
