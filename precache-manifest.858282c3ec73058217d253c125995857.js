a2491af705c9e975a04d6ca54481480c92657ea7
crisner <christie.reni@gmail.com>
2018-12-21 19:16:16 +0530

    Updates

---
+++ precache-manifest.858282c3ec73058217d253c125995857.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/myreads-react-project/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/myreads-react-project/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/myreads-react-project/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "ab45ea1af6743d879ff2",
+    "url": "/myreads-react-project/static/js/runtime~main.ab45ea1a.js"
+  },
+  {
+    "revision": "eb0ef1c1514d089686d1",
+    "url": "/myreads-react-project/static/js/main.eb0ef1c1.chunk.js"
+  },
+  {
+    "revision": "f462c235bf8a03925598",
+    "url": "/myreads-react-project/static/js/1.f462c235.chunk.js"
+  },
+  {
+    "revision": "eb0ef1c1514d089686d1",
+    "url": "/myreads-react-project/static/css/main.378f434b.chunk.css"
+  },
+  {
+    "revision": "65c16a0e5e293b49a938d37d02e9e271",
+    "url": "/myreads-react-project/index.html"
+  }
+];
