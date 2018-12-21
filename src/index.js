c64029db8a267ad6e869fbdf71702dce51741c10
crisner <christie.reni@gmail.com>
2018-12-21 19:46:48 +0530

    Remove history prop

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<BrowserRouter history={history} basename={process.env.PUBLIC_URL}>><App /></BrowserRouter>, document.getElementById('root'))
+ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}>><App /></BrowserRouter>, document.getElementById('root'))
